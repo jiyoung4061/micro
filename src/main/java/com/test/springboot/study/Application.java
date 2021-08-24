0f5366ae69ca8d4d77b9820674d7d0fdee8e4ea9
Ohjiyoung <jiyoung4061@naver.com>
2021-08-24 11:50:27 +0900

    initial commit

---
+++ src/main/java/com/test/springboot/study/Application.java
@@ -0,0 +1,14 @@
+package com.test.springboot.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 1-4. 패키지를 만든다
+// build.gradle 파일이 변경되면 무조건 Sync를 해줘야한다.
+// Auto Import : Alt + Enter
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Aplication.class, args);
+    }
+}
